a2ddbad0aa9fee325e5e361972cdd34522374d15
i3 <vinyk.vn@gmail.com>
2021-03-11 13:42:04 +0200

    Basic structure

--- src/main/java/ua/donnu/ta/Runner.java
+++ src/main/java/ua/donnu/ta/Runner.java
@@ -2,6 +2,16 @@ package ua.donnu.ta;
 
 public class Runner {
     public static void main(String[] args) {
+        double a = 5;
+        double b = 2;
+
+        double s = a * b * Math.PI;
+
+        System.out.println(s);
+
+        double p = 4*(Math.PI*a*b+(a-b))/a+b;
+
+        System.out.println(p);
 
     }
 }
